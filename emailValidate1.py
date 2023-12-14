59be5b85991566fdf0afa0005026a17bab1f875c
Amar Narote <113687074+amarnarote01@users.noreply.github.com>
2023-12-14 14:21:21 +0530

    Delete emailValidate1.py

--- emailValidate1.py
+++
@@ -1,7 +0,0 @@
-#  Write a program that accepts an email string and validates that email. 
-# If email is valid must display success message and If email is not valid must display error message
-# Example:  Input - test01*gmail.com
-#  	     Output - Invalid Email Id
-#  	     Input - student01@gmail.com
-#  	     Output - Valid Email Id
-
